9d2c44a2dbb93b6660889e4eb5f9977c38df6644
farhanrizkyafandi <farhanrizkyafandi@365.ugm.ac.id>
2019-09-24 10:52:50 +0700

    Add project files.

---
+++ Protoype Auto-Correct/Search.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Protoype_Auto_Correct
+{
+    class Search
+    {
+        public void Cari()
+        {
+            StreamReader sr = new StreamReader("ser.txt");
+            Console.WriteLine(sr.ReadLine());
+            sr.Close();
+        }
+    }
+}
