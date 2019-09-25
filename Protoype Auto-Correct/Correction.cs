7d43310fb5cff5e7dcbe20fcc2345aca8d88ef24
MEvanAW <evananindya@mail.ugm.ac.id>
2019-09-25 14:47:23 +0700

    Tambah Delete() dan Substitute()

--- Protoype Auto-Correct/Correction.cs
+++ Protoype Auto-Correct/Correction.cs
@@ -8,33 +8,44 @@ namespace Protoype_Auto_Correct
 {
     class Correction
     {
-
         public string Masuk(string kata)
         {
-            List<string[]> listString = Splits(kata);
+            List<string[]> listArrayString = Split(kata);
             string text = "Splits:" + Environment.NewLine;
-            foreach (string[] arrayString in listString)
+            foreach (string[] arrayString in listArrayString)
                 text += arrayString[0] + "-" + arrayString[1] + " ";
             text += Environment.NewLine + "Insert:" + Environment.NewLine;
-            List<string> listInsert = Insert(listString);
+            List<string> listInsert = Insert(listArrayString);
             foreach (string s in listInsert)
                 text += s + " ";
+            text += Environment.NewLine + "Delete:" + Environment.NewLine;
+            List<string> listDelete = Delete(listArrayString);
+            foreach (string s in listDelete)
+                text += s + " ";
+            text += Environment.NewLine + "Substitute:" + Environment.NewLine;
+            List<string> listSubstitute = Substitute(listArrayString);
+            foreach (string s in listSubstitute)
+                text += s + " ";
+            //text += Environment.NewLine + "Transpose:" + Environment.NewLine;
+            //List<string> listTranspose = Transpose(listArrayString);
+            //foreach (string s in listTranspose)
+            //    text += s + " ";
             return text;
         }
 
-        public List<string[]> Splits(string kata)
+        public List<string[]> Split(string kata)
         {
             int n = kata.Count();
-            List<string[]> listString = new List<string[]>();
+            List<string[]> listArrayString = new List<string[]>();
             string kata1 = "";
             string kata2 = "";
-            for(int i = 0 ; i <= n ; i++)
+            for (int i = 0 ; i <= n ; i++)
             {
                 kata1 = kata.Substring(0, i);
                 kata2 = kata.Substring(i, n - i);
-                listString.Add(new string[] { kata1, kata2 });
+                listArrayString.Add(new string[] { kata1, kata2 });
             }
-            return listString;
+            return listArrayString;
         }
 
         public List<string> Insert(List<string[]> kata)
@@ -47,5 +58,45 @@ namespace Protoype_Auto_Correct
             }
             return listString;
         }
+
+        public List<string> Delete(List<string[]> kata)
+        {
+            kata.RemoveAt(0);
+            int n = kata.Count();
+            List<string> listString = new List<string>();
+            for (int i = 0; i < n; i++)
+                kata[i][0] = kata[i][0].Remove(kata[i][0].Count() - 1);
+            foreach (string[] arrayString in kata)
+                listString.Add(arrayString[0] + arrayString[1]);
+            return listString;
+        }
+
+        public List<string> Substitute(List<string[]> kata)
+        {
+            int n = kata.Count();
+            List<string> listString = new List<string>();
+            foreach (string[] arrayString in kata)
+            {
+                for (char c = 'a'; c <= 'z'; c++)
+                    listString.Add(arrayString[0] + c + arrayString[1]);
+            }
+            return listString;
+        }
+
+        //public List<string> Transpose(List<string[]> kata)
+        //{
+        //    List<string> listString = new List<string>();
+        //    kata.RemoveAt(0);
+        //    kata.RemoveAt(kata.Count() - 1);
+        //    string s1 = "";
+        //    string s2 = "";
+        //    foreach (string[] arrayString in kata)
+        //    {
+        //        s1 = arrayString[0].Substring(arrayString[0].Count() - 1);
+        //        s2 = arrayString[1].Substring(0, 1);
+        //        listString.Add(arrayString[0].Remove(arrayString[0].Count() - 1) + s2 + s1 + arrayString[1].Remove(0, 1));
+        //    }
+        //    return listString;
+        //}
     }
 }
--- Protoype Auto-Correct/Form1.Designer.cs
+++ Protoype Auto-Correct/Form1.Designer.cs
@@ -35,16 +35,18 @@
             // 
             // key
             // 
-            this.key.Location = new System.Drawing.Point(169, 40);
+            this.key.Location = new System.Drawing.Point(190, 13);
+            this.key.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.key.Name = "key";
-            this.key.Size = new System.Drawing.Size(266, 22);
+            this.key.Size = new System.Drawing.Size(299, 26);
             this.key.TabIndex = 0;
             // 
             // enter
             // 
-            this.enter.Location = new System.Drawing.Point(360, 106);
+            this.enter.Location = new System.Drawing.Point(405, 47);
+            this.enter.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.enter.Name = "enter";
-            this.enter.Size = new System.Drawing.Size(75, 23);
+            this.enter.Size = new System.Drawing.Size(84, 29);
             this.enter.TabIndex = 1;
             this.enter.Text = "Enter";
             this.enter.UseVisualStyleBackColor = true;
@@ -52,21 +54,24 @@
             // 
             // hasil
             // 
-            this.hasil.Location = new System.Drawing.Point(12, 143);
+            this.hasil.Location = new System.Drawing.Point(14, 84);
+            this.hasil.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.hasil.Name = "hasil";
-            this.hasil.Size = new System.Drawing.Size(776, 305);
+            this.hasil.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.hasil.Size = new System.Drawing.Size(1043, 554);
             this.hasil.TabIndex = 2;
             this.hasil.TabStop = false;
             this.hasil.Text = "groupBox1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1069, 640);
             this.Controls.Add(this.hasil);
             this.Controls.Add(this.enter);
             this.Controls.Add(this.key);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
